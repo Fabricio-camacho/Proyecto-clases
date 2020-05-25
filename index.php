b2648a9650125e854f7b92fe80f62248ddf79aff
Fabricio-camacho <65914090+Fabricio-camacho@users.noreply.github.com>
2020-05-25 11:50:33 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,295 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Carniceria San Alfonso</title>
+     <link rel="shortcut icon" href="assets/img/Log.jpg" type="image/x-icon" />
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+  <link href="assets/css/flexslider.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />    
+    
+</head>
+<body >
+   
+ <div class="navbar navbar-inverse navbar-fixed-top " id="menu">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                
+          <h4>Carniceria San Alfonso</h4>
+            </div>
+            <div class="navbar-collapse collapse move-me">
+                <ul class="nav navbar-nav navbar-right">
+                    <li ><a href="#home">Inicio</a></li>
+            <li><a href="#course-sec">Contactanos</a></li>
+                     
+                </ul>
+            </div>
+           
+        </div>
+    </div>
+     
+       <div class="home-sec" id="home" >
+           <div class="overlay">
+ <div class="container">
+           <div class="row text-center " >
+           
+               <div class="col-lg-12  col-md-12 col-sm-12">
+               
+                <div class="flexslider set-flexi" id="main-section" >
+                    <ul class="slides move-me">
+                        
+                        <li>
+                              <h3>La mejor carniceria de Tlaquepaque</h3>
+                           <h1>Calidad en productos y precios</h1>
+                            <a  href="#features-sec" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#ln">
+                                Cliente
+                            </a>
+                             <a  href="#features-sec" class="btn btn-success btn-lg" data-toggle="modal" data-target="#su">
+                                Registrarse
+                            </a>
+              
+              <a  href="#features-sec" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#an">
+                                Administrador
+                            </a>
+                        </li>
+                        
+                        <li>
+                            <h3>La mejor carniceria de Tlaquepaque</h3>
+                           <h1>La carne mas fresca de la ciudad</h1>
+                             <a  href="#features-sec" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#ln">
+                               Cliente
+                            </a>
+                             <a  href="#features-sec" class="btn btn-success btn-lg" data-toggle="modal" data-target="#su">
+                                Registrarse
+                            </a>
+              
+              <a  href="#features-sec" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#an">
+                                administrador
+                            </a>
+                        </li>
+                        
+                        <li>
+                            <h3>La mejor carniceria de Tlaquepaque</h3>
+                           <h1>Carne de la mejor calidad</h1>
+                             <a  href="#features-sec" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#ln">
+                                Cliente
+                            </a>
+                             <a  href="#features-sec" class="btn btn-success btn-lg" data-toggle="modal" data-target="#su">
+                                Registrarse
+                            </a>
+              <a  href="#features-sec" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#an">
+                                Administrador
+                        </a>
+                      </li>
+                  </ul>
+                </div>
+              </div> 
+            </div>
+          </div>
+        </div>
+           
+       </div>
+         
+    <div  class="tag-line" >
+         <div class="container">
+           <div class="row  text-center" >
+           
+               <div class="col-lg-12  col-md-12 col-sm-12">
+               
+        <h2 data-scroll-reveal="enter from the bottom after 0.1s" ><i class="fa fa-circle-o-notch"></i> Bienvenidos a carniceria San Alfonso<i class="fa fa-circle-o-notch"></i> </h2>
+                   </div>
+               </div>
+             </div>
+
+      <div id="course-sec" class="container set-pad">
+             <div class="row text-center">
+                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                     <h1 data-scroll-reveal="enter from the bottom after 0.1s" class="header-line">Contactanos</h1>
+                     <p data-scroll-reveal="enter from the bottom after 0.3s">
+                      Si tienes alguna duda sobre nuestros productos o tiendas ponte en contacto con nosostros para ayudarte.
+             <br />Estamos a sus ordenes.
+             </p>
+             </div>
+             </div>
+             <br />
+           
+             <div class="container">
+             <div class="row set-row-pad"  >
+    <div class="col-lg-4 col-md-4 col-sm-4   col-lg-offset-1 col-md-offset-1 col-sm-offset-1 " data-scroll-reveal="enter from the bottom after 0.4s">
+
+                    <h2 ><strong> Estamos ubicados en </strong></h2>
+        <hr />
+                    <div>
+                        <h4>Calle Guadalupe Victoria #42</h4>
+                        <h4>Ha tres cuadras del templo de San Alfonso</h4>
+             
+                    </div>
+
+
+                </div>
+                 <div class="col-lg-4 col-md-4 col-sm-4   col-lg-offset-1 col-md-offset-1 col-sm-offset-1" data-scroll-reveal="enter from the bottom after 0.4s">
+
+                    <h2 ><strong> Contactos </strong></h2>
+        <hr />
+                    <div >
+                        <h4><strong>Telefono:</strong>  +51 555 444 333 </h4>
+                        <h4><strong>Correo Electronico: </strong>carnessanalfonso@gmail.com</h4>
+                    </div>
+                    </div>
+
+
+                </div>
+                 </div>
+                 
+                 
+               </div>
+             </div>
+  
+     <div class="modal fade" id="su" tabindex="-1" role="dialog" aria-labelledby="myMediulModalLabel">
+          <div class="modal-dialog modal-sm">
+            <div style="color:white;background-color:#008CBA" class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel"> Registrate aqui</h4>
+              </div>
+              <div class="modal-body">
+            
+        
+         <form role="form" method="post" action="register.php">
+                   <fieldset>
+          
+              <div class="form-group">
+                                <input class="form-control" placeholder="Nombre" name="ruser_firstname" type="text" required>
+              </div>
+              
+              <div class="form-group">
+                                <input class="form-control" placeholder="Apellido" name="ruser_lastname" type="text" required>
+              </div>
+              
+              <div class="form-group">
+                                <input class="form-control" placeholder="Direccion" name="ruser_address" type="text" required>
+              </div>
+              
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Correo electronico" name="ruser_email" type="email" required>
+              </div>
+              
+              <div class="form-group">
+                                <input class="form-control" placeholder="Contraseña" name="ruser_password" type="password" required>
+              </div>
+           
+           </fieldset>
+                  
+            
+              </div>
+              <div class="modal-footer">
+               
+                <button class="btn btn-md btn-warning btn-block" name="register">Registrarse</button>
+        
+         <button type="button" class="btn btn-md btn-success btn-block" data-dismiss="modal">Cancelar</button>
+           </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+     <div class="modal fade" id="ln" tabindex="-1" role="dialog" aria-labelledby="myMediulModalLabel">
+          <div class="modal-dialog modal-sm">
+            <div style="color:white;background-color:#008CBA" class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 style="color:white" class="modal-title" id="myModalLabel">Iniciar sesion</h4>
+              </div>
+              <div class="modal-body">
+            
+        
+         <form role="form" method="post" action="userlogin.php">
+                   <fieldset>
+          
+            
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Correo electronico" name="user_email" type="email" required>
+              </div>
+              
+              <div class="form-group">
+                                <input class="form-control" placeholder="Contraseña" name="user_password" type="password" required>
+              </div>
+           
+           </fieldset>
+                  
+            
+              </div>
+              <div class="modal-footer">
+               
+                <button class="btn btn-md btn-warning btn-block" name="user_login">Ingresar</button>
+        
+         <button type="button" class="btn btn-md btn-success btn-block" data-dismiss="modal">Cancelar</button>
+           </form>
+           
+              </div>
+            </div>
+          </div>
+        </div>
+    
+    <div class="modal fade" id="an" tabindex="-1" role="dialog" aria-labelledby="myMediulModalLabel">
+          <div class="modal-dialog modal-sm">
+           <div style="color:white;background-color:#008CBA" class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 style="color:white" class="modal-title" id="myModalLabel">Usuario Administrador</h4>
+              </div>
+              <div class="modal-body">
+            
+        
+         <form role="form" method="post" action="adminlogin.php">
+                   <fieldset>
+          
+            
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Usuario" name="admin_username" type="text" required>
+              </div>
+              
+              <div class="form-group">
+                                <input class="form-control" placeholder="Contraseña" name="admin_password" type="password" required>
+              </div>
+           
+           </fieldset>
+                  
+            
+              </div>
+              <div class="modal-footer">
+               
+                <button class="btn btn-md btn-warning btn-block" name="admin_login">Iniciar Sesion</button>
+        
+         <button type="button" class="btn btn-md btn-success btn-block" data-dismiss="modal">Cancelar</button>
+           </form>
+              </div>
+            </div>
+          </div>
+        </div>
+     <br />
+       <br />
+       <br />
+      
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/jquery.flexslider.js"></script>
+    <script src="assets/js/scrollReveal.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/custom.js"></script> 
+   
+
+</body>
+</html>
